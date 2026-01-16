a033f0b8ec0943ca6759752d3b522a7fa9fd82aa
Valerito <danielvalerogenil@gmail.com>
2026-01-16 10:40:09 +0100

    Delete HTML directory

--- HTML/js_functionalities/audioSettingsAndPlay.js
+++
@@ -1,3 +0,0 @@
-    const audio = document.getElementById('bgAudio');
-    audio.volume = 0.35;
-    audio.playbackRate = 1.0;
--- HTML/js_functionalities/reloj.js
+++
@@ -1,9 +0,0 @@
-function actualizarReloj() {
-    const ahora = new Date();
-    const horas = String(ahora.getHours()).padStart(2, '0');
-    const minutos = String(ahora.getMinutes()).padStart(2, '0');
-
-    document.getElementById('reloj').textContent = `${horas}:${minutos}`;
-}
-setInterval(actualizarReloj, 1000);
-actualizarReloj();
